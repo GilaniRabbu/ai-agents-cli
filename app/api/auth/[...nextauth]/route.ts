f8f4f2efb29d5d2685234a2817f4c0710d005f4a
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-07-07 00:50:25 +0600

    updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -10,6 +9,15 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      console.log(url, baseUrl);
+      return "https://ai-agents-cli.vercel.app/ai-agents";
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
